b04586446ecc419577df9307d03899055409ed40
davidionita <31045833+davidionita@users.noreply.github.com>
2020-03-08 12:18:47 -0400

    Initial Commit

---
+++ AutismLampApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AutismLampApp
+//
+//  Created by David Ionita on 3/8/20.
+//  Copyright © 2020 David Ionita. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
